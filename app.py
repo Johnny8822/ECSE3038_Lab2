f9d8de05d6d259b30d7bfe69d1e5a852d6ecdd3d
Johnny8822 <146615330+Johnny8822@users.noreply.github.com>
2024-02-10 23:43:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-data = [
-    "name"
-] 
+data = [] 
 
-@app.post("/names") 
-async def create_name(data: names)
-    return  
-   data.append(datum[names])
+@app.post("/person") 
+async def create_person(person):
+    if not person.get("occupation") or not person.get("name") or not person.get("address"):
+        result = {"error_message": "invalid request"}    
+        print(result)
+        return result
+    else:
+        data.append(person)
+        result = "success" 
+        print(data)
+        return result
+        
 
+@app.get("/person")
+def get_person():
+    return data
