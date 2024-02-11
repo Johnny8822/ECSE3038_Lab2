102fac6820ef32c37ae8b627b036b581b3457aec
Johnny8822 <146615330+Johnny8822@users.noreply.github.com>
2024-02-10 23:02:06 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+data = [
+    "name"
+] 
+
+@app.post("/names") 
+async def create_name(data: names)
+    return  
+   data.append(datum[names])
+
